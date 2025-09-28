6eaef7018a8d7da64df4cf42ed9b6f189279c480
dedrowe <barushnikovvs@gmail.com>
2025-09-28 17:13:26 +0400

    feature: добавил дополнительные методы в FileSystemService

--- src/main/java/org/example/tonpad/core/files/FileSystemService.java
+++ src/main/java/org/example/tonpad/core/files/FileSystemService.java
@@ -7,17 +7,33 @@ public interface FileSystemService {
 
     FileTree getFileTree(String path);
 
+    FileTree getFileTree(Path path);
+
     List<String> getAllFilesInDir(String directory);
+
+    List<String> getAllFilesInDir(Path directory);
 
     Path makeDir(String directory);
 
+    Path makeDir(Path directory);
+
     Path makeFile(String path);
+
+    Path makeFile(Path path);
 
     List<String> readFile(String path);
 
+    List<String> readFile(Path path);
+
     void write(String path, List<String> content);
+
+    void write(Path path, List<String> content);
 
     Path rename(String oldPath, String newPath);
 
+    Path rename(Path oldPath, Path newPath);
+
     void delete(String path);
+
+    void delete(Path path);
 }
