b70a2de9a2b64ffe8032453ca60737c13b100adb
dedrowe <barushnikovvs@gmail.com>
2025-11-21 00:06:05 +0400

    refactor: убрал создание алертов при ошибках шифрования, перевел текст ошибок на английский язык

--- src/main/java/org/example/tonpad/core/service/crypto/EncryptionService.java
+++ src/main/java/org/example/tonpad/core/service/crypto/EncryptionService.java
@@ -2,13 +2,18 @@ package org.example.tonpad.core.service.crypto;
 
 import java.nio.file.Path;
 
-import org.example.tonpad.core.service.crypto.exception.DecryptionException;
-import org.example.tonpad.core.service.crypto.exception.EncryptionException;
+import org.example.tonpad.core.exceptions.DecryptionException;
+import org.example.tonpad.core.exceptions.EncryptionException;
 
 public interface EncryptionService {
+
     String encrypt(String text, String aad) throws EncryptionException;
+
     byte[] encrypt(byte[] text, byte[] aad) throws EncryptionException;
+
     String decrypt(String text, String aad) throws DecryptionException;
+
     byte[] decrypt(byte[] text, byte[] aad) throws DecryptionException;
+
     boolean isOpeningWithNoPasswordAllowed(Path path);
 }
--- src/main/java/org/example/tonpad/core/service/crypto/exception/EncryptionException.java
+++
@@ -1,5 +0,0 @@
-package org.example.tonpad.core.service.crypto.exception;
-
-public class EncryptionException extends Exception {
-    public EncryptionException(Exception e) {super(e);}
-}
