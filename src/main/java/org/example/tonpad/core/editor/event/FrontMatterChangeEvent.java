23c4ccc0e7388ac241ee434b1a0e1235a6a7da06
pavel <tomashaytis@gmail.com>
2025-11-20 22:35:39 +0400

    refactor: небольшие изменения

--- src/main/java/org/example/tonpad/core/editor/event/FrontMatterChangeEvent.java
+++ src/main/java/org/example/tonpad/core/editor/event/FrontMatterChangeEvent.java
@@ -1,26 +1,9 @@
 package org.example.tonpad.core.editor.event;
 
-public class FrontMatterChangeEvent {
-    private final String action; // "add", "updateValue", "updateKey", "delete"
-    private final String oldKey;
-    private final String oldValue;
-    private final String newKey;
-    private final String newValue;
+import lombok.Getter;
 
-    public FrontMatterChangeEvent(String action, String oldKey, String oldValue, String newKey, String newValue) {
-        this.action = action;
-        this.oldKey = oldKey;
-        this.oldValue = oldValue;
-        this.newKey = newKey;
-        this.newValue = newValue;
-    }
-
-    // Getters
-    public String getAction() { return action; }
-    public String getOldKey() { return oldKey; }
-    public String getOldValue() { return oldValue; }
-    public String getNewKey() { return newKey; }
-    public String getNewValue() { return newValue; }
+@Getter
+public record FrontMatterChangeEvent(String action, String oldKey, String oldValue, String newKey, String newValue) {
 
     public String getAffectedKey() {
         return switch (action) {
